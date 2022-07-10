641e5a755cf4299e90358cfb3e4382e0aeb38719
Florence Shen <fshen408@student.fuhsd.org>
2022-07-10 11:41:03 -0700

    Initial Commit

---
+++ foodlake/foodlakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodlakeApp.swift
+//  foodlake
+//
+//  Created by Florence on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct foodlakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
